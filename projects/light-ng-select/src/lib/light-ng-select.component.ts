c3af992355380a6926b7b3889626061147d2dd47
mojtabanpe <43112914+mojtabanpe@users.noreply.github.com>
2022-09-15 23:21:13 +0430

    add basic functionality

---
+++ projects/light-ng-select/src/lib/light-ng-select.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'light-ng-select',
+  templateUrl: './light-ng-select.component.html',
+  styleUrls: ['./light-ng-select.component.css']
+})
+export class LightNGSelectComponent implements OnInit {
+  @Input() keys: Array<any> = [];
+  @Input() selectedKey: any;
+  isOpen = false;
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if(!this.eRef.nativeElement.contains(event.target)) {  
+      this.isOpen = false;
+    }
+  }
+
+  constructor(private eRef: ElementRef) { }
+
+  ngOnInit(): void {
+    if(!this.selectedKey) {
+      this.selectedKey = this.keys[0];
+    }
+  }
+
+  changeSelectedKey(item: any): void {
+    this.selectedKey = item;
+    this.isOpen = false;
+  }
+
+}
